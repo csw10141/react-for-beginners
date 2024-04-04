899210e1eeab42aed3269d79aaa376fc3de3b9a7
csw10141 <cjstjddn10@naver.com>
2024-04-04 09:18:47 +0900

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	modified:   src/components/Movie.js 	modified:   src/routes/Detail.js 	modified:   src/routes/Home.js

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,25 +1,26 @@
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-function Movie({coverImage, title, summary, genres}){
-    return (
-        <div>
-        <img src={coverImage} alt={title} />
-        <h2>{title}</h2>
-        <p>{summary}</p>
-        <ul>
-          {genres.map((g) => (
-            <li key={g}>{g}</li>
-          ))}
-        </ul>
-      </div>
-    );
+function Movie({ coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <Link to="/movie"><h2>{title}</h2></Link>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-Movie.PropTypes = {
-    coverImg: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    summary: PropTypes.string.isRequired,
-    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+Movie.propTypes = {
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,40 +1,39 @@
 import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
+import Movie from "../components/Movie";
 
-function Home(){
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const getMovies = async() => {
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
     const json = await (
-        await fetch(
+      await fetch(
         `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-        )
-        ).json();
+      )
+    ).json();
     setMovies(json.data.movies);
     setLoading(false);
-    };
-    useEffect(() => {
+  };
+  useEffect(() => {
     getMovies();
-    }, []);
-
-    return (
+  }, []);
+  return (
     <div>
-        {loading ? (
-        <h3>Loading....</h3>
-        ) : (
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
         <div>
-            {movies.map((movie) => (
-            <Movie 
-                key={movie.id}
-                coverImage={movie.medium_cover_image} 
-                title={movie.title} 
-                summary={movie.summary} 
-                genres={movie.genres} />
-            ))}
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
         </div>
-        )}
+      )}
     </div>
-    );
+  );
 }
-
-export default Home();
+export default Home;
